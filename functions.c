7e70011cd375be99564579611af15cd79134fd9f
untalinfo <racso1607@gmail.com>
2020-03-16 23:00:00 +0000

    the function p_int was added, this function printed the integer or decimal)

--- functions.c
+++ functions.c
@@ -15,3 +15,28 @@ int p_string(va_list list)
 {
 	return(_puts(va_arg(list, char *)));
 }
+int p_int(va_list list)
+{
+	int integer, div = 1;
+	int len_i = 0;
+	unsigned int num;
+
+	integer = va_arg(list, int);
+
+	if (integer < 0)
+	{
+		len_i = len_i + _putchar('-');
+		num = integer * -1;
+	}
+	else
+		num = integer;
+	while (num / div > 9)
+		div = div * 10;
+	while (div != 0)
+	{
+		len_i = len_i + _putchar('0' + num / div);
+		num = num % div;
+		div = div / 10;
+	}
+	return (len_i);	
+}
